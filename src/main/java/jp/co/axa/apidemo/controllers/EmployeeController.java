12e2f6f434b0f07102396832e8ef3c7543a73227
Ashok Murthy <ashokmurthy88@gmail.com>
2021-07-08 18:39:26 +0900

    formatted code

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -25,30 +25,29 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
+    public Employee getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
         return employeeService.getEmployee(employeeId);
     }
 
     @PostMapping("/employees")
-    public void saveEmployee(Employee employee){
+    public void saveEmployee(Employee employee) {
         employeeService.saveEmployee(employee);
         System.out.println("Employee Saved Successfully");
     }
 
     @DeleteMapping("/employees/{employeeId}")
-    public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
+    public void deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
         employeeService.deleteEmployee(employeeId);
         System.out.println("Employee Deleted Successfully");
     }
 
     @PutMapping("/employees/{employeeId}")
     public void updateEmployee(@RequestBody Employee employee,
-                               @PathVariable(name="employeeId")Long employeeId){
+                               @PathVariable(name = "employeeId") Long employeeId) {
         Employee emp = employeeService.getEmployee(employeeId);
-        if(emp != null){
+        if (emp != null) {
             employeeService.updateEmployee(employee);
         }
 
     }
-
 }
